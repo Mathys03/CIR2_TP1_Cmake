afb431aeab50d1a8a2026dab53e8aacb22e67348
PascalM <pascal.mosbah@kathab.fr>
2020-11-05 09:08:12 +0100

    test

--- main.cpp
+++ main.cpp
@@ -5,3 +5,20 @@ int main(){
 	std::cout << "Hello world\n";
 	return EXIT_SUCCESS;
 }
+
+
+#define TABSIZE 10
+#define TABSIZE1 5
+
+int main(){
+	int i, j;
+	int MyTab[TABSIZE][TABSIZE1];
+	for (i = 0;i<TABSIZE;++i){
+		for (j = 0;j<TABSIZE1;++j){
+			MyTab[i][j] = i + j * 10;
+	}
+	for (i=0; i<TABSIZE * TABSIZE1; i++) {
+		 printf("%d ",*(MyTab+i));
+	}
+
+}
